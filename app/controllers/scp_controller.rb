221db3f20a570d588ff2d6e27817d04d5de5c9d7
Krasi2405 <krasi2405@gmail.com>
2019-05-29 22:40:14 +0300

    Update SCP's

--- app/controllers/scp_controller.rb
+++ app/controllers/scp_controller.rb
@@ -49,7 +49,49 @@ class ScpController < ApplicationController
     @anomaly_classes = ActiveRecord::Base.connection.execute(class_query)
   end
 
+  def edit
+    clearance_query = "SELECT Level, Name FROM SecurityClearance;"
+    @clearance_levels = ActiveRecord::Base.connection.execute(clearance_query)
+
+    class_query = "SELECT Id, Name FROM AnomalyClass;"
+    @anomaly_classes = ActiveRecord::Base.connection.execute(class_query)
+
+    id = params[:id]
+
+
+    query = " SELECT scp.Id, scp.Name, scp.Description, scp.SecurityClearanceNeeded, scp.ClassId 
+              FROM SCP
+              WHERE scp.Id = ?;"
+
+    vals = [[nil, id]]
+
+    @scp = ActiveRecord::Base.connection.exec_insert(query, "update", vals)[0]
+  end
+
+
   def update
+    id = params[:id]
+    name = params[:name]
+    description = params[:description]
+    clearance_level = params[:clearance_level]
+    anomaly_class = params[:anomaly_class]
+
+    query = " UPDATE SCP
+              SET 
+              Name = ?,
+              Description = ?,
+              SecurityClearanceNeeded = ?,
+              ClassId = ?
+              WHERE Id = ?"
+
+    vals = [[nil, name], [nil, description], [nil, clearance_level], [nil, anomaly_class], [nil, id]]
+
+    begin
+      ActiveRecord::Base.connection.exec_insert(query, "create", vals)
+      redirect_to controller: "scp", action: "show", id: id
+    rescue ActiveRecord::NotNullViolation => e
+      redirect_to controller: "scp", action: "edit", id: id
+    end
   end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,8 @@ get 'scp', to: 'scp#index'
   get 'scp/show'
   get 'scp/create'
   post 'scp/new'
-  get 'scp/update'
+  get 'scp/edit'
+  post 'scp/update'
   get 'scp/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
